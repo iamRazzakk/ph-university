afa2f8397763892f5739cc2b834e4cd7bdc064d7
iamRazzakk <rakibt23p@gmail.com>
2024-07-17 17:37:12 -0400

    Set Cookies In The Browser

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "./features/auth/authSlice"
+import { baseApi } from './Api/baseApi';
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     auth: authReducer
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware)
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
